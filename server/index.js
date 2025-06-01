404a8b7e070b11bd961c239a1643788785919672
shiv-pratap04 <pratapsinghs801@gmail.com>
2025-06-01 23:20:58 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,11 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-<<<<<<< HEAD
-		origin:"*",
-=======
-		origin:["http://localhost:3000","https://edtech-platform-ap9lxl2ws-shiv-pratap04s-projects.vercel.app"],
->>>>>>> 16682ed (Update index.js)
+		origin:["http://localhost:3000","https://edtech-platform-abx6gpd4i7-shiv-pratap04s-projects.vercel.app"],
 		credentials:true,
 	})
 )
